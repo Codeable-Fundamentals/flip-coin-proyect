7f975ba71e8f1ca49faa5d1f9e63590c211c79f9
Grecia Delgado <greciadm2596@gmail.com>
2025-08-07 20:05:27 -0600

    update code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const button = document.getElementById("flip-button");
 
 const result = document.getElementById("result");
 
+const coinImage = document.getElementById("coin");
+
 function flipTheCoin() {
   const textResult = Math.random() < 0.5 ? "Heads" : "Tails";
 
   result.textContent = textResult;
+
+  const isHeads = textResult === "Heads";
+  
+  coinImage.src = isHeads ? "./assets/coin-head.png" : "./assets/coin-tail.png";
 }
 
 button.addEventListener("click", flipTheCoin);
-
-
