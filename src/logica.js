ef412da1203a5ad5f9ef232b90ea7fa84934730e
FabricioCP1806 <fabriciojcorper2@gmail.com>
2025-08-07 21:10:47 -0500

    Estilo giratorio aplicado

--- src/logica.js
+++ src/logica.js
@@ -2,14 +2,22 @@ function flipCoin(){
     console.log("x");
     let coin = document.getElementById("coin"); //<img>
     let p = document.querySelector("#result");
-    let result = Math.random() < 0.5 ? "Heads" : "Tails"; //<p>
-    if (result=="Heads"){
-        coin.src = "./assets/coin-head.png";
-        p.innerHTML = "Head";
-    } else {
-        coin.src = "./assets/coin-tail.png";
-        p.innerHTML = "Tail";
-    }
+
+    coin.classList.remove("flip");
+    void coin.offsetWidth;
+    coin.classList.add("flip");
+
+    setTimeout(() =>{
+        const result = Math.random() < 0.5 ? "Heads" : "Tails"; //<p>
+        if (result=="Heads"){
+            coin.src = "./assets/coin-head.png";
+            p.innerHTML = "Head";
+        } else {
+            coin.src = "./assets/coin-tail.png";
+            p.innerHTML = "Tail";
+        }
+    },1000);
+    
 }
 
 const boton = document.getElementById("flip-button");
