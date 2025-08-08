fb350c09e318af3dc10879134ad74d6b33383c37
rgap <r.guzmanap@gmail.com>
2025-08-07 20:42:16 -0500

    Add script.js file
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    
    On branch rel-guzman
    Changes to be committed:
    	modified:   src/index.html
    	new file:   src/script.js

---
+++ src/script.js
@@ -0,0 +1,11 @@
+function flipCoin() {
+  const result = Math.random() < 0.5 ? "Heads" : "Tails";
+
+  if (result === "Tails") {
+    document.querySelector("#coin").src = "./assets/coin-tail.png";
+    document.querySelector("#result").textContent = "tails";
+  } else {
+    document.querySelector("#coin").src = "./assets/coin-head.png";
+    document.querySelector("#result").textContent = "heads";
+  }
+}
